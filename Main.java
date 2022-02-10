d52717c95c22b879922bc0d26efb84bdce465fb7
Marusya <manya-cat@rambler.ru>
2022-02-10 19:13:06 +0700

    Среднее арифметическое

--- Main.java
+++ Main.java
@@ -5,11 +5,19 @@ public class Main {
 
         int[] sequence = {10, 11, 12, 13, 14, 15};
         double average = 0;
-
+        int count = 0;
+        int sum = 0;
         for (int i : sequence) {
             average += i;
         }
-
         System.out.println("Среднее арифметическое: " + average / sequence.length);
+
+        for (int j = 0; j < sequence.length; j++) {
+            if (sequence[j] % 2 == 0) {
+                sum += sequence[j];
+                count++;
+            }
+        }
+        System.out.println("Среднее арифметическое чётных чисел: " + sum / count);
     }
 }
