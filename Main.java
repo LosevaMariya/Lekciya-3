b29d67ec1e85cdde264b580c4f4fe3c5887f927e
Marusya <manya-cat@rambler.ru>
2022-02-12 10:12:12 +0700

    Среднее арифметическое

--- Main.java
+++ Main.java
@@ -1,23 +1,30 @@
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
 
-        int[] sequence = {10, 11, 12, 13, 14, 15};
-        double average = 0;
-        int count = 0;
-        int sum = 0;
-        for (int i : sequence) {
-            average += i;
-        }
-        System.out.println("Среднее арифметическое: " + average / sequence.length);
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Введите начало диапазона от 10 до 20: ");
+        double a = scanner.nextInt();
+        System.out.print("Введите конец диапазона от 10 до 20: ");
+        double b = scanner.nextInt();
+        double count = 0;
+        double count1 = 0;
+        double sum = 0;
+        double sum1 = 0;
 
-        for (int j = 0; j < sequence.length; j++) {
-            if (sequence[j] % 2 == 0) {
-                sum += sequence[j];
-                count++;
+        for (int i = 10; i >= a && i <= b; i++, count++) {
+            sum += i;
+        }
+        System.out.println("Среднее арифметическое: " + sum / count);
+
+        for (int j = 10; j >= a && j <= b; j++) {
+            if (j % 2 == 0) {
+                sum1 += j;
+                count1++;
             }
         }
-        System.out.println("Среднее арифметическое чётных чисел: " + sum / count);
+        System.out.println("Среднее арифметическое чётных чисел: " + sum1 / count1);
     }
 }
