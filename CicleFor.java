4da391093e2eba93201216a5bb1f8d34c970f519
Marusya <manya-cat@rambler.ru>
2022-02-12 10:52:17 +0700

    Цикл for. Не получается, нет кнопки запуска + удалила лишние переносы строк

--- CicleFor.java
+++ CicleFor.java
@@ -7,15 +7,11 @@ public class CicleFor {
         Scanner scanner = new Scanner(System.in);
         int a = 1;
         int b = 100;
-
-//        System.out.println("Массив: " + );
     }
-
     public static void multiplicityFour(int a, int b) {
         for (int j = 100; j >= a && j <= b; j--) {
             if (j % 4 == 0) {
                 System.out.println("Числа, кратные 4: ");
-
             }
         }
     }
