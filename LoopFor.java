43d577e7893dcb657b097931ca3311ade6785010
Marusya <manya-cat@rambler.ru>
2022-02-15 08:00:09 +0700

    Мой безобразный код, в котором я сама не могу разобраться

---
+++ LoopFor.java
@@ -0,0 +1,15 @@
+public class LoopFor {
+
+    public static void main(String[] args) {
+
+         for (k = 100, isMultipleFour(boolean k) = true, k--){
+             int multiplicityFour = isMultipleFour(boolean k);
+//        if (a = isMultipleFour(int k)){
+                System.out.print("Числа, кратные 4: " + multiplicityFour);
+            }
+        }
+//    }
+    public static boolean isMultipleFour(int k){
+        return for(k = 100; k % 4 == 0; k--);
+    }
+}
