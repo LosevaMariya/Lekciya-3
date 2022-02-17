f0f28b7b269b693f495bb4835bed8b118de08f47
Marusya <manya-cat@rambler.ru>
2022-02-17 07:34:59 +0700

    Число, которое ввели в консоли, взято по модулю

--- DigitsOfNumber.java
+++ DigitsOfNumber.java
@@ -5,13 +5,13 @@ public class DigitsOfNumber {
     public static void main(String[] args) {
 
         Scanner scanner1 = new Scanner(System.in);
-        System.out.print("введите целое число: ");
+        System.out.print("Введите целое число: ");
         int a = scanner1.nextInt();
         int sum = 0;
         int sum1 = 0;
         int max = 0;
 
-        for (int n = a; n > 0; n /= 10) {
+        for (int n = Math.abs(a); n != 0; n /= 10) {
             sum += (n % 10);
             if (n % 2 != 0) {
                 sum1 += (n % 10);
@@ -19,9 +19,6 @@ public class DigitsOfNumber {
             if (max < (n % 10)) {
                 max = (n % 10);
             }
-//            else {
-//                System.out.println("Введёное число должно быть положительным ");
-//            }
         }
         System.out.println("Сумма цифр числа: " + sum);
         System.out.println("Сумма нечётных цифр числа: " + sum1);
